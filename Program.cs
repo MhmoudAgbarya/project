f4d832a2d93704b9e78b42367203fda2053cd423
Mhmoud Agbarya <agbaryamhmoud@gmail.com>
2022-07-06 00:09:03 +0300

    w

--- Program.cs
+++ Program.cs
@@ -8,14 +8,7 @@ internal class Program
 
         client.DefaultRequestHeaders.Add("User-Agent", "C# console program");
 
-        // var content = await client.GetStringAsync("http://webcode.me");
         var content = await client.GetStringAsync("https://www.espn.com/");
-    //     var str = Regex.Replace(content, "(<style.+?</style>)|(<script.+?</script>)", " ", RegexOptions.IgnoreCase | RegexOptions.Singleline);
-    //     str = Regex.Replace(str, "(<img.+?>)", " ", RegexOptions.IgnoreCase | RegexOptions.Singleline);
-    //     str = Regex.Replace(str, "(<o:.+?</o:.+?>)", " ", RegexOptions.IgnoreCase | RegexOptions.Singleline);
-    //     str = Regex.Replace(str, "<!--.+?-->", " ", RegexOptions.IgnoreCase | RegexOptions.Singleline);
-    //     str = Regex.Replace(str, "class=.+?>", ">", RegexOptions.IgnoreCase | RegexOptions.Singleline);
-    //     str = Regex.Replace(str, "class=.+?\\s", " ", RegexOptions.IgnoreCase | RegexOptions.Singleline);
 
 
        

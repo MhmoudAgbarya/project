7cd1def744d428b668801f75d395946ff0d9b372
Mhmoud Agbarya <agbaryamhmoud@gmail.com>
2022-07-06 00:07:40 +0300

    WIP

--- Program.cs
+++ Program.cs
@@ -4,21 +4,32 @@ internal class Program
 {
     private static async Task Main(string[] args)
     {
-        // Console.WriteLine("Hello, World!");
         using var client = new HttpClient();
 
         client.DefaultRequestHeaders.Add("User-Agent", "C# console program");
 
-        var content = await client.GetStringAsync("http://webcode.me");
+        // var content = await client.GetStringAsync("http://webcode.me");
+        var content = await client.GetStringAsync("https://www.espn.com/");
+    //     var str = Regex.Replace(content, "(<style.+?</style>)|(<script.+?</script>)", " ", RegexOptions.IgnoreCase | RegexOptions.Singleline);
+    //     str = Regex.Replace(str, "(<img.+?>)", " ", RegexOptions.IgnoreCase | RegexOptions.Singleline);
+    //     str = Regex.Replace(str, "(<o:.+?</o:.+?>)", " ", RegexOptions.IgnoreCase | RegexOptions.Singleline);
+    //     str = Regex.Replace(str, "<!--.+?-->", " ", RegexOptions.IgnoreCase | RegexOptions.Singleline);
+    //     str = Regex.Replace(str, "class=.+?>", ">", RegexOptions.IgnoreCase | RegexOptions.Singleline);
+    //     str = Regex.Replace(str, "class=.+?\\s", " ", RegexOptions.IgnoreCase | RegexOptions.Singleline);
 
-       var str = Regex.Replace(content, "<.*?>", "!").Split("!");
 
-        // Console.WriteLine(str);
-        // var str = content.Replace(@"<[^>]*>", "");
-        for(int c = 0; c < str.Length; c++){
+       
+       var str = Regex.Replace(content, @"<script[^>]*>[\s\S]*?</script>", " ");
+       str = Regex.Replace(str, @"<style[^>]*>[\s\S]*?</style>", " ");
+       str = Regex.Replace(content, "<.*?>", " ");
+       str = Regex.Replace(str, @"\s+", " ");
 
-        Console.WriteLine(str[c]);
-        Console.WriteLine("xxxxxxxxxxxxxx");
-        }
+    
+
+       var doesInclude = str.Contains("img");
+
+        Console.WriteLine(str);
+        Console.WriteLine(doesInclude);
+
     }
 }

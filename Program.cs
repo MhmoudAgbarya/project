adaebd2b597f4b2e518cf894ff55285941d0ab6b
hadeel <hadeel.jab.96@hotmail.com>
2022-07-08 00:27:39 +0300

    hh

--- Program.cs
+++ Program.cs
@@ -11,9 +11,11 @@ internal class Program
 
         var url = System.Uri.UriSchemeHttps;
 
-        var content = await client.GetStringAsync("https://www.javatpoint.com/c-sharp-multidimensional-array");
-
-
+        var content = await client.GetStringAsync("https://www.javatpoint.com/");//تخزين محتويات الموقع
+        
+        // Console.WriteLine("enter the web: ");// ادخال الموقع
+        // var web = Console.ReadLine();
+        // var content = await client.GetStringAsync(web);//تخزين محتويات الموقع
        
        var str = Regex.Replace(content, @"<script[^>]*>[\s\S]*?</script>", " ");
        str = Regex.Replace(str, @"<style[^>]*>[\s\S]*?</style>", " ");
@@ -22,28 +24,28 @@ internal class Program
        var strArr = str.Split("newWord");
 
 
-       if (File.Exists("bad words.txt.txt"))
+       if (File.Exists("bad words.txt.txt"))//فحص اذا كان الملف موجود أم لا؟
         {
-            int badWordsCounter = 0;
-            StreamReader badwords = new StreamReader("bad words.txt.txt");
-            var badWordArray = badwords.ReadLine()?.Split("/");
-            
-            string [,] badwordsmatrix=new string [badWordArray.Length,2];
+            int badWordsCounter = 0;//عداد كم مرة تكررت الكلمة
+            StreamReader badwords = new StreamReader("bad words.txt.txt");//قراءة الكلمات من الملف
+            var badWordArray = badwords.ReadLine()?.Split("/");//تخزين الكلمات بعد القراءة
 
-            if (badWordArray != null)
+            string [,] badwordsmatrix=new string [badWordArray.Length,2];//بناء مصفوفة ثنائية الابعاد لحفظ الكلمات وكم مرة تكررت
+
+            if (badWordArray != null)//فحص اذا كان كلمات؟
             {
-                for(int k=0; k<badWordArray.Length; k++)
+                for(int k=0; k<badWordArray.Length; k++)//تخزين الكلمات في المصفوفة ثنائية الابعاد
                 {
                     badwordsmatrix[k,0]=badWordArray[k];
                     badwordsmatrix[k,1]="0";
                 }
-                for(int i=0; i< strArr.Length; i++)
+                for(int i=0; i< strArr.Length; i++)//
                 {
                     var temp = strArr[i];
                     var doesInclude = Array.Find(badWordArray,element=>element == strArr[i]);
-                    if(doesInclude != null)
+                    if(doesInclude != null)// عداد كم مرة تكررت كل كلمة
                     {
-                        for(int g=0; g<badWordArray.Length;g++)
+                        for(int g=0; g<badWordArray.Length;g++)//؟
                         {
                             if(badwordsmatrix[g,0]== badWordArray[g])
                             {
@@ -54,17 +56,16 @@ internal class Program
                         }
                         badWordsCounter++;
                         doesInclude = null;
-                    }
+                    }// تخزين العداد لكل كلمة بالمصفوفة ثنائية الابعاد
                     
                 }
-                for(int m=0; m<badWordArray.Length;m++)
+                for(int m=0; m<badWordArray.Length;m++)// طباعة الكلمات وكم مرة تكررت
                 {
-                    Console.Write(badwordsmatrix[m,0]);
+                    Console.Write(" {0} : ",badwordsmatrix[m,0]);
                     Console.WriteLine(badwordsmatrix[m,1]);
-                    
                 }
             }
-            badwords.Close();
+            badwords.Close();// اغلاق ملف الكلمات
         }
     
 

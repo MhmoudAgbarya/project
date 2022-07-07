c52f02760a6291eba3bf31f5db86e1e5b019ee57
hadeel <hadeel.jab.96@hotmail.com>
2022-07-07 16:23:52 +0300

    dgd

--- Program.cs
+++ Program.cs
@@ -10,9 +10,8 @@ internal class Program
         client.DefaultRequestHeaders.Add("User-Agent", "C# console program");
 
         var url = System.Uri.UriSchemeHttps;
-        //  HttpContext.Current.Request.Url.AbsoluteUri;
 
-        var content = await client.GetStringAsync("https://www.espn.com/");
+        var content = await client.GetStringAsync("https://www.javatpoint.com/c-sharp-multidimensional-array");
 
 
        
@@ -22,37 +21,50 @@ internal class Program
        str = Regex.Replace(str, @"\s+", "newWord");
        var strArr = str.Split("newWord");
 
-       //لازم اعمل for داخلية
-       //ا بتفحص اذا موجودة الكلمة، بتمرق على كل النص عشان اعرف كم مرة تكررت
 
-        //    bool x = false;
        if (File.Exists("bad words.txt.txt"))
         {
             int badWordsCounter = 0;
-            //webSiteCounter = 0;
             StreamReader badwords = new StreamReader("bad words.txt.txt");
             var badWordArray = badwords.ReadLine()?.Split("/");
+            
+            string [,] badwordsmatrix=new string [badWordArray.Length,2];
+
             if (badWordArray != null)
             {
+                for(int k=0; k<badWordArray.Length; k++)
+                {
+                    badwordsmatrix[k,0]=badWordArray[k];
+                    badwordsmatrix[k,1]="0";
+                }
                 for(int i=0; i< strArr.Length; i++)
                 {
                     var temp = strArr[i];
                     var doesInclude = Array.Find(badWordArray,element=>element == strArr[i]);
                     if(doesInclude != null)
                     {
+                        for(int g=0; g<badWordArray.Length;g++)
+                        {
+                            if(badwordsmatrix[g,0]== badWordArray[g])
+                            {
+                                Int32.TryParse(badwordsmatrix[g,1], out int j);
+                                j++;
+                                badwordsmatrix[g,1]=j.ToString();
+                            }
+                        }
                         badWordsCounter++;
                         doesInclude = null;
                     }
-                    Console.WriteLine(badWordsCounter);
+                    
                 }
-
+                for(int m=0; m<badWordArray.Length;m++)
+                {
+                    Console.Write(badwordsmatrix[m,0]);
+                    Console.WriteLine(badwordsmatrix[m,1]);
+                    
+                }
             }
             badwords.Close();
-            // if (x == false)// الكلمة مش موجودة
-            // {
-            //     Console.WriteLine("the word is not found!!!");
-            //     Console.ReadKey();
-            // }
         }
     
 
